65c95f75dff1516b0ae204374e2ea9ed0823ef65
Fynn Thierling <fynnth@outlook.de>
2025-03-19 19:56:00 +0100

    CCS-22 Update Streamlit UI test configurations and improve test reliability

--- tests/UI/test_streamlit_ui.py
+++ tests/UI/test_streamlit_ui.py
@@ -1,5 +1,7 @@
 from streamlit.testing.v1 import AppTest
-
+import os
 
 def test_streamlit_ui():
-    _ = AppTest.from_file("src\\NoKeeA\\UI\\streamlit_ui.py").run()
+    ui_path = os.getenv("STREAMLIT_UI_SCRIPT_TEST",
+                        "src\\NoKeeA\\UI\\streamlit_ui.py")
+    _ = AppTest.from_file(ui_path).run()
