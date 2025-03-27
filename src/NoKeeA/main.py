cc6b8e0e2825780160dc371ab8887bb9ed742e5f
ComTols <comtols@outlook.de>
2025-03-27 17:11:09 +0100

    CCS-27 Updating text recognition
    This commit improves the usability of text recognition by downloading the model when the application starts and making it permanently available offline.

--- src/NoKeeA/main.py
+++ src/NoKeeA/main.py
@@ -1,8 +1,12 @@
 from NoKeeA.UI import start_ui
+from huggingface_hub import snapshot_download
 
 
 def main():
     """Start the NoKeeA application"""
+    snapshot_download(repo_id="Salesforce/blip2-opt-2.7b",
+                      local_dir="blip2_model")
+
     streamlit_process = start_ui()
     streamlit_process.wait()  # Wait for the process to complete
     return streamlit_process
